9b953d177006dc040473cab9c01520ba6ffb61cd
Dalip Dalip -X (ddalip - ARICENT TECHNOLOGIES MAURITIUS LIMITED at Cisco) <ddalip@partnet.cisco.com>
2019-06-27 18:23:29 +0530

    sum in file

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-console.log("hello world");
+'use strict';
 
+const express = require('express');
+const fs = require('fs');
+
+// Constants
+const PORT = 8085;
+const HOST = 'localhost';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+
+  fs.readFile(`${__dirname}/sum.txt`, 'utf-8', (err, data) => {
+        if (!err) {
+          let result = 0;
+
+          if (data === '0') {
+            result = 5;
+          }
+          else {
+            result = (parseInt(data) + 5);
+          }
+
+          fs.writeFile(`${__dirname}/sum.txt`, result, (error) => {
+            if (error) {
+              console.log('error:', error);
+            }
+          });
+        }
+      });
+
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
